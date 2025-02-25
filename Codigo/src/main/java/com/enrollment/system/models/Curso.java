6d20dc2cfa1d960a4e38af0c727458c82e3f18cf
Kimberly Liz Spencer Lourenço <kimberlylizsl@gmail.com>
2025-02-25 17:02:19 -0300

    fix: create curriculo and curso models #18

---
+++ Codigo/src/main/java/com/enrollment/system/models/Curso.java
@@ -0,0 +1,35 @@
+package com.enrollment.system.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="curso")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private int creditos;
+
+    @OneToMany
+    @JoinColumn(name = "disciplina_id")
+    private List<Disciplina> disciplinas;
+
+}
