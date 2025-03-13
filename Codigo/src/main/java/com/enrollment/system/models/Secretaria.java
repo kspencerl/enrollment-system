5e9d1b22676e96e95a9a7ebe46bc41d86fcb0617
Joana Morais <joanaalvesm@gmail.com>
2025-03-12 22:16:52 -0300

    feat: implement Secretaria migration & model.

--- Codigo/src/main/java/com/enrollment/system/models/Secretaria.java
+++ Codigo/src/main/java/com/enrollment/system/models/Secretaria.java
@@ -2,13 +2,21 @@ package com.enrollment.system.models;
 
 import com.enrollment.system.enums.TipoUsuario;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="secretaria")
+@Table(name = "secretaria")
 @PrimaryKeyJoinColumn(name = "id_usuario")
-@DiscriminatorValue("secretaria")
-public class Secretaria extends Usuario {}
+public class Secretaria extends Usuario {
 
+    @OneToOne
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id", insertable = false, updatable = false)
+    private Usuario usuario;
+
+    public Secretaria() {
+    }
+
+    public Secretaria(Long id, String nome, String email, String senha, TipoUsuario tipo) {
+        super(id, nome, email, senha, tipo);
+    }
+
+}
